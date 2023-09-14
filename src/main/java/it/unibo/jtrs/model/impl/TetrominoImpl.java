932d8c69d38fdf314ffe6275664311c288b83d1b
Sonofapo <github@sonofapo.slmail.me>
2023-09-14 12:48:09 +0200

    Code cleanup

--- src/main/java/it/unibo/jtrs/model/impl/TetrominoImpl.java
+++ src/main/java/it/unibo/jtrs/model/impl/TetrominoImpl.java
@@ -14,15 +14,7 @@ public class TetrominoImpl implements Tetromino {
 
 	public TetrominoImpl(Set<Pair<Integer, Integer>> initialComponents) {
 		this.compontents = new HashSet<>(initialComponents);
-
-		int maxX = this.compontents.stream().mapToInt(Pair::getX).max().getAsInt();
-		int minX = this.compontents.stream().mapToInt(Pair::getX).min().getAsInt();
-		int maxY = this.compontents.stream().mapToInt(Pair::getY).max().getAsInt();
-		int minY = this.compontents.stream().mapToInt(Pair::getY).min().getAsInt();
-
-		int wh = (int) Math.ceil((maxX - minX) / 2.0);
-		int hh = (int) Math.ceil((maxY - minY) / 2.0);
-		this.pivot = new Pair<>(minX + wh, minY + hh);
+		this.pivot = this.center();
 	}
 
 	@Override
@@ -35,7 +27,7 @@ public class TetrominoImpl implements Tetromino {
 
 	@Override
 	public void translate(int x, int y) {
-		this.center();
+		this.pivot = this.center();
 		this.compontents = this.compontents.stream()
 			.map(c -> new Pair<>(c.getX() + x, c.getY() + y))
 			.collect(Collectors.toSet());
@@ -46,7 +38,7 @@ public class TetrominoImpl implements Tetromino {
 		return Set.copyOf(this.compontents);
 	}
 
-	private void center() {
+	private Pair<Integer, Integer> center() {
 		int maxX = this.compontents.stream().mapToInt(Pair::getX).max().getAsInt();
 		int minX = this.compontents.stream().mapToInt(Pair::getX).min().getAsInt();
 		int maxY = this.compontents.stream().mapToInt(Pair::getY).max().getAsInt();
@@ -54,7 +46,7 @@ public class TetrominoImpl implements Tetromino {
 
 		int wh = (int) Math.ceil((maxX - minX) / 2.0);
 		int hh = (int) Math.ceil((maxY - minY) / 2.0);
-		this.pivot = new Pair<>(minX + wh, minY + hh);
+		return new Pair<>(minX + wh, minY + hh);
 	}
 	
 }
