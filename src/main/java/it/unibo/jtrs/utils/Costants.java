d9e716c56d9929dce7e0001f7c5819f658a08514
Alex giamperoli <giamperolial@gmail.com>
2023-09-14 16:25:26 +0200

    Added initial frame

---
+++ src/main/java/it/unibo/jtrs/utils/Costants.java
@@ -0,0 +1,13 @@
+package it.unibo.jtrs.utils;
+
+public class Costants {
+    
+    public static final int FRAME_WIDTH = 800;
+
+    public static final int FRAME_HEIGHT = 900;
+
+    public static final int GRID_WIDTH = 800;
+
+    public static final int GRID_HEIGHT = 900;
+
+}
--- src/main/java/it/unibo/jtrs/view/impl/GamePanelImpl.java
+++ src/main/java/it/unibo/jtrs/view/impl/GamePanelImpl.java
@@ -1,19 +1,33 @@
 package it.unibo.jtrs.view.impl;
 
-import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import it.unibo.jtrs.controller.api.Application;
 import it.unibo.jtrs.view.api.GamePanel;
 
-public class GamePanelImpl extends JPanel implements GamePanel {
+public class GamePanelImpl extends JFrame implements GamePanel {
 
-    public GamePanelImpl(final int width, final int height) {
-        setPreferredSize(new Dimension(width, height));
+    private final Application application;
+
+    public GamePanelImpl(final int width, final int height, final Application application) {
+        this.application = application;
+        
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(width, height);
+        this.setTitle("jTetris");
+        this.setResizable(false);
+        this.setVisible(true);
+
+        JPanel background = new JPanel(new GridLayout(1,2));
+        this.getContentPane().add(background);
+
     }
 
     @Override
     public void redraw() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'redraw'");
     }
     
 }
