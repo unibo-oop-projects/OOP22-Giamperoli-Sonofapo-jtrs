0f9d800348c715de707fdc215f701e0a4d0ea8f2
Sonofapo <github@sonofapo.slmail.me>
2023-09-14 12:29:00 +0200

    Tetromino implementation and initial test

---
+++ src/main/java/it/unibo/jtrs/model/impl/TetrominoImpl.java
@@ -0,0 +1,60 @@
+package it.unibo.jtrs.model.impl;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import it.unibo.jtrs.model.api.Tetromino;
+import it.unibo.jtrs.utils.Pair;
+
+public class TetrominoImpl implements Tetromino {
+
+	private Set<Pair<Integer, Integer>> compontents;
+	private Pair<Integer, Integer> pivot;
+
+	public TetrominoImpl(Set<Pair<Integer, Integer>> initialComponents) {
+		this.compontents = new HashSet<>(initialComponents);
+
+		int maxX = this.compontents.stream().mapToInt(Pair::getX).max().getAsInt();
+		int minX = this.compontents.stream().mapToInt(Pair::getX).min().getAsInt();
+		int maxY = this.compontents.stream().mapToInt(Pair::getY).max().getAsInt();
+		int minY = this.compontents.stream().mapToInt(Pair::getY).min().getAsInt();
+
+		int wh = (int) Math.ceil((maxX - minX) / 2.0);
+		int hh = (int) Math.ceil((maxY - minY) / 2.0);
+		this.pivot = new Pair<>(minX + wh, minY + hh);
+	}
+
+	@Override
+	public void rotate() {
+		this.compontents = this.compontents.stream()
+			.map(c -> new Pair<>(c.getY() - pivot.getY() + pivot.getX(),
+				pivot.getX() - c.getX() + pivot.getY()))
+			.collect(Collectors.toSet());
+	}
+
+	@Override
+	public void translate(int x, int y) {
+		this.center();
+		this.compontents = this.compontents.stream()
+			.map(c -> new Pair<>(c.getX() + x, c.getY() + y))
+			.collect(Collectors.toSet());
+	}
+
+	@Override
+	public Set<Pair<Integer, Integer>> getComponents() {
+		return Set.copyOf(this.compontents);
+	}
+
+	private void center() {
+		int maxX = this.compontents.stream().mapToInt(Pair::getX).max().getAsInt();
+		int minX = this.compontents.stream().mapToInt(Pair::getX).min().getAsInt();
+		int maxY = this.compontents.stream().mapToInt(Pair::getY).max().getAsInt();
+		int minY = this.compontents.stream().mapToInt(Pair::getY).min().getAsInt();
+
+		int wh = (int) Math.ceil((maxX - minX) / 2.0);
+		int hh = (int) Math.ceil((maxY - minY) / 2.0);
+		this.pivot = new Pair<>(minX + wh, minY + hh);
+	}
+	
+}
---
+++ src/test/java/it/unibo/jtrs/TetrominoTest.java
@@ -0,0 +1,40 @@
+package it.unibo.jtrs;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.jtrs.model.api.Tetromino;
+import it.unibo.jtrs.model.impl.TetrominoImpl;
+import it.unibo.jtrs.utils.Pair;
+
+public class TetrominoTest {
+
+	private Tetromino tetromino;
+	private final Set<Pair<Integer, Integer>> initialComponents =
+		Set.of(new Pair<>(0,0), new Pair<>(0,1), new Pair<>(1,1), new Pair<>(2,1));
+
+	@BeforeAll
+	public void init() {
+		tetromino = new TetrominoImpl(initialComponents);
+	}
+
+	@Test
+	public void testRotate() {
+		tetromino.rotate();
+		assertEquals(tetromino.getComponents(),
+			Set.of(new Pair<>(1,2), new Pair<>(1,1), new Pair<>(1,0), new Pair<>(0,2)));
+		tetromino.rotate();
+		assertEquals(tetromino.getComponents(),
+			Set.of(new Pair<>(2,2), new Pair<>(1,1), new Pair<>(2,1), new Pair<>(0,1)));
+		tetromino.rotate();
+		assertEquals(tetromino.getComponents(),
+			Set.of(new Pair<>(1,2), new Pair<>(1,1), new Pair<>(1,0), new Pair<>(2,0)));
+		tetromino.rotate();
+		assertEquals(tetromino.getComponents(), initialComponents);
+	}
+	
+}
