0c664b87d0599281bcfecfba588a3f675da7b5b8
Sonofapo <github@sonofapo.slmail.me>
2023-09-15 14:39:03 +0200

    Typo and docs

---
+++ src/main/java/it/unibo/jtrs/utils/Constants.java
@@ -0,0 +1,36 @@
+package it.unibo.jtrs.utils;
+
+/**
+ * A class containing all useful constant used in this application
+ */
+public class Constants {
+
+	// prevents instantiation
+	private Constants() {} 
+
+	/**
+	 * Main application window width.
+	 */
+	public static final int FRAME_WIDTH = 800;
+	/**
+	 * Main application window height.
+	 */
+	public static final int FRAME_HEIGHT = 900;
+	/**
+	 * Gameboard vertical cells.
+	 */
+	public static final int GRID_ROWS = 20;
+	/**
+	 * Gameboard horizontal cells.
+	 */
+	public static final int GRID_COLS = 10;
+	/**
+	 * Preview panel vertical cells.
+	 */
+	public static final int PREVIEW_ROWS = 5;
+	/**
+	 * Preview panel horizontal cells.
+	 */
+	public static final int PREVIEW_COLS = 5;
+
+}
