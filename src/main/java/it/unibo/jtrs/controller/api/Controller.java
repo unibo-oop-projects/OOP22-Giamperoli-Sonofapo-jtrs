031c234e42e56594424de54293fa22ab8f4ef057
Sonofapo <github@sonofapo.slmail.me>
2023-09-15 11:23:04 +0200

    Updated controller and model interface

--- src/main/java/it/unibo/jtrs/controller/api/Controller.java
+++ src/main/java/it/unibo/jtrs/controller/api/Controller.java
@@ -1,6 +1,10 @@
 package it.unibo.jtrs.controller.api;
 
+import it.unibo.jtrs.view.api.SubPanel;
+
 public interface Controller<X> {
 	
-	public X update();
+	public X update(X data);
+
+	public SubPanel<X> getView();
 }
---
+++ src/main/java/it/unibo/jtrs/model/api/Model.java
@@ -0,0 +1,9 @@
+package it.unibo.jtrs.model.api;
+
+public interface Model<X> {
+
+	public X get();
+
+	public void set(X data);
+	
+}
