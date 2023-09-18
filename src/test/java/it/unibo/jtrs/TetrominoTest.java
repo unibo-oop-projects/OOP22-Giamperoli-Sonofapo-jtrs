79b64372826ac8502587d0b70a7fa982362394a0
Sonofapo <github@sonofapo.slmail.me>
2023-09-18 09:51:49 +0200

    Checkstyle and PMD

--- src/test/java/it/unibo/jtrs/TetrominoTest.java
+++ src/test/java/it/unibo/jtrs/TetrominoTest.java
@@ -12,17 +12,27 @@ import it.unibo.jtrs.model.impl.TetrominoImpl;
 import it.unibo.jtrs.utils.Pair;
 import it.unibo.jtrs.utils.TetrominoData;
 
+/**
+ * Tetromino test class.
+ */
+@SuppressWarnings("PMD")
 public class TetrominoTest {
 
     private Tetromino tetromino;
     private final Set<Pair<Integer, Integer>> initialComponents =
         Set.of(new Pair<>(0, 0), new Pair<>(0, 1), new Pair<>(1, 1), new Pair<>(2, 1));
 
+    /**
+     * Initialize necessary field for the tests.
+     */
     @BeforeEach
     public void init() {
         tetromino = new TetrominoImpl(initialComponents, TetrominoData.O_COLOR);
     }
 
+    /**
+     * Test Tetromino rotation.
+     */
     @Test
     public void testRotate() {
         tetromino.rotate();
