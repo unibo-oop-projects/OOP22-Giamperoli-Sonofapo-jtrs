8624ea3a1bce4c707fed74f81feb585e243eaef1
Sonofapo <github@sonofapo.slmail.me>
2023-09-23 14:58:48 +0200

    Added background

--- src/main/java/it/unibo/jtrs/view/impl/ApplicationPanel.java
+++ src/main/java/it/unibo/jtrs/view/impl/ApplicationPanel.java
@@ -1,13 +1,18 @@
 package it.unibo.jtrs.view.impl;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import it.unibo.jtrs.controller.api.Application;
 import it.unibo.jtrs.game.core.inputs.KeyboardReader;
+import it.unibo.jtrs.utils.ResourceLoader;
 
 /**
  * A class modelling the main panel to be inserted in a frame.
@@ -21,6 +26,7 @@ public class ApplicationPanel extends JPanel {
     private final PreviewPanel previewPanel;
     private final ScorePanel scorePanel;
     private final GamePanel gamePanel;
+    private transient BufferedImage background;
 
     /**
      * Constructor.
@@ -35,7 +41,7 @@ public class ApplicationPanel extends JPanel {
 
         this.setLayout(new GridLayout(1, 2));
         this.setBorder(new EmptyBorder(PADDING, PADDING, PADDING, PADDING));
-        this.setBackground(Color.blue);
+        this.setBackground(Color.DARK_GRAY);
 
         final var panelL = new JPanel(new GridLayout());
         final var panelR = new JPanel(new GridLayout(2, 1));
@@ -49,6 +55,12 @@ public class ApplicationPanel extends JPanel {
 
         this.addKeyListener(new KeyboardReader(application));
         this.setFocusable(true);
+
+        try {
+            this.background = ImageIO.read(ResourceLoader.load("background.jpg"));
+        } catch (IOException e) {
+            this.background = null;
+        }
     }
 
     /**
@@ -58,5 +70,16 @@ public class ApplicationPanel extends JPanel {
         this.gamePanel.redraw();
         this.previewPanel.redraw();
         this.scorePanel.redraw();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void paintComponent(final Graphics g) {
+        super.paintComponent(g);
+        if (this.background != null) {
+            g.drawImage(this.background, 0, 0, this);
+        }
     }
 }
