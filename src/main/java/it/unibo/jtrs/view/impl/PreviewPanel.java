0b4954e32235d1d11064368154a60941c3e7c751
Sonofapo <github@sonofapo.slmail.me>
2023-09-23 14:27:40 +0200

    Updated preview panel

--- src/main/java/it/unibo/jtrs/view/impl/PreviewPanel.java
+++ src/main/java/it/unibo/jtrs/view/impl/PreviewPanel.java
@@ -1,16 +1,18 @@
 package it.unibo.jtrs.view.impl;
 
 import java.awt.Color;
-import java.awt.GridLayout;
 import java.util.stream.Collectors;
 
-import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.EtchedBorder;
 
 import it.unibo.jtrs.controller.impl.PreviewController;
-import it.unibo.jtrs.view.api.GridPanel;
 import it.unibo.jtrs.view.api.View;
+import it.unibo.jtrs.view.custom.GridPanel;
+import it.unibo.jtrs.view.custom.Label;
 
 /**
  * The class models the preview panel. This view must show the next Tetromino.
@@ -22,6 +24,7 @@ public class PreviewPanel extends JPanel implements View {
     private static final int GRID_COLS = 5;
     private static final int GRID_ROWS = 4;
     private static final int PADDING = 100;
+    private static final float FONT_SIZE = 30f;
 
     private final GridPanel preview;
     private final transient PreviewController controller;
@@ -34,12 +37,19 @@ public class PreviewPanel extends JPanel implements View {
     public PreviewPanel(final PreviewController controller) {
         this.controller = controller;
 
-        this.preview = new GridPanel(GRID_ROWS, GRID_COLS, PADDING);
-        this.setLayout(new GridLayout());
-        this.add(this.preview);
-        this.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
         this.setOpaque(false);
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.add(Box.createVerticalStrut(PADDING));
+
+        this.preview = new GridPanel(GRID_ROWS, GRID_COLS, 0);
         this.preview.setOpaque(false);
+        this.preview.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+
+        final JLabel label = new Label("NEXT", "Tetris.ttf", FONT_SIZE, Color.WHITE);
+        label.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+
+        this.add(label);
+        this.add(this.preview);
     }
 
     /**
