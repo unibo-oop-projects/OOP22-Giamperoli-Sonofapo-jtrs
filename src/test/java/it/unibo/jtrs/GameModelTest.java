5c6aa38f682ff25b18ddc8fe575e8cb3edeb927a
Alex giamperoli <giamperolial@gmail.com>
2023-09-25 13:33:10 +0200

    Added next piece and delete rows tests

--- src/test/java/it/unibo/jtrs/GameModelTest.java
+++ src/test/java/it/unibo/jtrs/GameModelTest.java
@@ -1,13 +1,17 @@
 package it.unibo.jtrs;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import it.unibo.jtrs.model.api.GameModel;
+import it.unibo.jtrs.model.api.Tetromino;
 import it.unibo.jtrs.model.impl.GameModelImpl;
 import it.unibo.jtrs.model.impl.TetrominoImpl;
 import it.unibo.jtrs.utils.Pair;
@@ -16,8 +20,7 @@ import it.unibo.jtrs.utils.TetrominoData;
 /**
  * GameModel test class.
  */
-@SuppressWarnings("PMD")
-public class GameModelTest {
+class GameModelTest {
 
     private GameModel model;
 
@@ -26,26 +29,95 @@ public class GameModelTest {
      * Initialize necessary field for the tests.
      */
     @BeforeEach
-    public void init() {
+    void init() {
         this.model = new GameModelImpl();
+    }
+
+    /**
+     * Test next piece.
+     */
+    @Test
+    void testNextPiece() {
+        for (int i = 18; i >= 0; i = i - 2) {
+            assertTrue(this.model.nextPiece(new TetrominoImpl(TetrominoData.O_COORD, i, 0, TetrominoData.O_COLOR)));
+        }
+        assertFalse(this.model.nextPiece(new TetrominoImpl(TetrominoData.O_COORD, 0, 0, TetrominoData.O_COLOR)));
+    }
+
+    /**
+     * Test delete one row.
+     */
+    @Test
+    void testDeleteOneRow() {
+        this.model.nextPiece(new TetrominoImpl(TetrominoData.I_COORD, 18, 0, TetrominoData.I_COLOR));
+        this.model.nextPiece(new TetrominoImpl(TetrominoData.I_COORD, 18, 4, TetrominoData.I_COLOR));
+        this.model.nextPiece(new TetrominoImpl(TetrominoData.O_COORD, 18, 8, TetrominoData.O_COLOR));
+        assertEquals(1, this.model.deleteRows());
+        assertEquals(2, this.model.getPieces().size());
+        assertEquals(Set.of(new Pair<>(19, 8), new Pair<>(19, 9)), this.getAllComponents());
     }
 
     /**
      * Test delete two rows.
      */
     @Test
-    public void testDeleteTwoRows() {
-        this.model.nextPiece(new TetrominoImpl(TetrominoData.O_COORD, 18, 0, TetrominoData.O_COLOR));
-        this.model.nextPiece(new TetrominoImpl(TetrominoData.O_COORD, 18, 2, TetrominoData.O_COLOR));
-        this.model.nextPiece(new TetrominoImpl(TetrominoData.O_COORD, 18, 4, TetrominoData.O_COLOR));
-        this.model.nextPiece(new TetrominoImpl(TetrominoData.O_COORD, 18, 6, TetrominoData.O_COLOR));
-        this.model.nextPiece(new TetrominoImpl(TetrominoData.O_COORD, 18, 8, TetrominoData.O_COLOR));
+    void testDeleteTwoRows() {
+        for (int i = 0; i < 10; i = i + 2) {
+            this.model.nextPiece(new TetrominoImpl(TetrominoData.O_COORD, 18, i, TetrominoData.O_COLOR));
+        }
         this.model.nextPiece(new TetrominoImpl(TetrominoData.O_COORD, 16, 0, TetrominoData.O_COLOR));
 
         assertEquals(2, this.model.deleteRows());
         assertEquals(1, this.model.getPieces().size());
-        assertEquals(Set.of(new Pair<>(18, 0), new Pair<>(18, 1), new Pair<>(19, 0), new Pair<>(19, 1)),
-            this.model.getPieces().get(0).getComponents());
+        assertEquals(Set.of(new Pair<>(18, 0), new Pair<>(18, 1), 
+            new Pair<>(19, 0), new Pair<>(19, 1)), this.getAllComponents());
+    }
+
+    /**
+     * Test delete three rows.
+     */
+    @Test
+    void testDeleteThreeRows() {
+        for (int i = 0; i < 8; i = i + 2) {
+            this.model.nextPiece(new TetrominoImpl(TetrominoData.O_COORD, 18, i, TetrominoData.O_COLOR));
+        }
+        this.model.nextPiece(new TetrominoImpl(TetrominoData.I_COORD, 16, 0, TetrominoData.I_COLOR));
+        this.model.nextPiece(new TetrominoImpl(TetrominoData.I_COORD, 16, 4, TetrominoData.I_COLOR));
+
+        Tetromino tmp = new TetrominoImpl(TetrominoData.I_COORD, 16, 6, TetrominoData.I_COLOR);
+        tmp.rotate();
+        this.model.nextPiece(tmp);
+        tmp = new TetrominoImpl(TetrominoData.I_COORD, 16, 7, TetrominoData.I_COLOR);
+        tmp.rotate();
+        this.model.nextPiece(tmp);
+
+        assertEquals(3, this.model.deleteRows());
+        assertEquals(2, this.model.getPieces().size());
+        assertEquals(Set.of(new Pair<>(19, 8), new Pair<>(19, 9)), this.getAllComponents());
+    }
+
+    /**
+     * Test delete four rows.
+     */
+    @Test
+    void testDeleteFourRows() {
+        Tetromino tmp;
+        for (int i = 0; i < 10; i = i + 1) {
+            tmp = new TetrominoImpl(TetrominoData.I_COORD, 16, i - 2, TetrominoData.I_COLOR);
+            tmp.rotate();
+            this.model.nextPiece(tmp);
+        }
+        this.model.nextPiece(new TetrominoImpl(TetrominoData.O_COORD, 14, 0, TetrominoData.O_COLOR));
+        assertEquals(4, this.model.deleteRows());
+        assertEquals(1, this.model.getPieces().size());
+        assertEquals(Set.of(new Pair<>(19, 0), new Pair<>(19, 1), 
+            new Pair<>(18, 0), new Pair<>(18, 1)), this.getAllComponents());
     }
     // CHECKSTYLE: MagicNumber ON
+
+    Set<Pair<Integer, Integer>> getAllComponents() {
+        return this.model.getPieces().stream()
+                .flatMap(p -> p.getComponents().stream())
+                .collect(Collectors.toSet());
+    }
 }
