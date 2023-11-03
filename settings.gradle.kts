da2bd819fed06d3e0b5d8147599c6d342f4f7301
Danilo Pianini <danilo.pianini@unibo.it>
2023-11-03 17:15:36 +0100

    ci: add the OOP machinery

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = jtrs
+rootProject.name = "jtrs"
 plugins {
     id("com.gradle.enterprise") version "3.15.1"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
